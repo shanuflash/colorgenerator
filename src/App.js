f59e49a669082003704f1a340f84343ca9441a54
Shanu S <shanuflash@gmail.com>
2023-01-07 14:50:59 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* //hue: red, orange, yellow, green, blue, purple, pink and monochrome
+      //luminosity: light or dark 
+      //count: number 
+      //format: rgb, rgba, hsl, hsla and hex 
+      //https://www.npmjs.com/package/randomcolor */}
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
